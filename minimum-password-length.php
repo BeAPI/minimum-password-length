a0dd55167c1fb32a82e04729b3cb9c2b2bbe4788
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 15:26:54 +0200

    Update credentials

--- minimum-password-length.php
+++ minimum-password-length.php
@@ -1,12 +1,31 @@
 <?php
-
 /*
- * Plugin Name: Minimum Password Length
- * Description: Enforce a specific password length.
- * Version: 1.0.1
- * Plugin URI: http://wordpress.org/extend/plugins/minimum-password-length/
- * Author: BE API
- * Author URI: http://beapi.fr/
+ Plugin Name: Minimum Password Length
+ Version: 1.0.1
+ Plugin URI: http://www.beapi.fr
+ Description: Enforce a specific password length.
+ Author: BE API Technical team
+ Author URI: http://www.beapi.fr
+ Domain Path: languages
+ Text Domain: bea-plugin-boilerplate
+ 
+ ----
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
  class Minimum_Password_Length {
